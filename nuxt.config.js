f8d8b98b1134a250daaa64f5e5c9ed55e4860bf7
jvdeichmann <74736126+jvdeichmann@users.noreply.github.com>
2022-10-26 15:06:36 -0300

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,124 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'spa',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    '@nuxtjs/auth'
+  ],
+  /*
+   ** Auth module configuration
+   ** See https://auth.nuxtjs.org/api/options.html
+   */
+  auth: {
+    strategies: {
+      facebook: {
+        // donot add clientsecret/faceboooksecret anywhere as it will be visible on client side and will invite unwanted attention
+        client_id: 'Enter your facebook clientid here',
+        userinfo_endpoint:
+          'https://graph.facebook.com/me?fields=about,name,picture.typr(large){url},email,birthday',
+        scope: ['public_profile', 'email', 'user_birthday']
+      },
+      google: {
+        client_id: 'Enter your google clientid here'
+      }
+    },
+    redirect: {
+      login: '/registration',
+      logout: '/registration',
+      callback: '/registration',
+      home: '/'
+    },
+    /*
+     ** pluugins to extend the auth module
+     ** See https://auth.nuxtjs.org/recipes/extend.html
+     */
+    plugins: ['~/plugins/extendauth.js']
+  },
+  /*
+   ** Router configuration
+   ** See https://nuxtjs.org/api/configuration-router
+   */
+  router: {
+    middleware: ['auth']
+  },
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-card class="pa-5" width="500">
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar size="10">
+            <img
+              :src="
+                $store.state.auth.strategy === 'google'
+                  ? $auth.user.picture
+                  : $auth.user.picture.data.url
+              "
+            />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title class="ma-2 font-weight-bold">{{
+              $auth.user.name
+            }}</v-list-item-title>
+            <v-list-item-subtitle class="ma-2 font-weight-bold">{{
+              $auth.user.email
+            }}</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon height="50" width="50" @click="$auth.logout()">
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-layout>
+</template>
